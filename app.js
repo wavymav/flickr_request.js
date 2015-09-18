9c92481281e98c5dc1b9dc75dcf3ae73f82b6e63
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-18 13:39:53 -0400

    added callback() to .getJSON()

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ $(document).ready(function() {
 		$('button').removeClass('selected');
 		// Then adds the css .sleceted to the button element that was clicked
 		$(this).addClass('selected');
+
 		// getJSON() method takes 3 parameters
+
 		// 1) url
 		// Note: '?jsoncallback=?' is used to make JSONP requests as workaround for server security
 		var flickerApiUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?',
+
 		// 2) data request
 				// Gets the inner text value of the button that was clicked
 				animal = $(this).text(),
@@ -22,10 +25,37 @@ $(document).ready(function() {
 					// Allow to request my prefered data format ex(JSON or XML)
 					format: 'json'
 				};
-		// 3) callback function()
-			// takes the returned parsed json data
-		$.getJSON(flickerApiUrl, flickerData, function(data) {
 
+		// 3) callback function()
+		// takes the returned parsed json data to display photos
+		/* callback function(data) {
+			var thumbnailTemplate = '<ul>';
+			$.each(data.items, function(index, photo){
+				thumbnailTemplate += '<li class="grid-25 tablet-grid-50">' +
+																'<a href="' + photo.link +'" class="image">' +
+																	'<img src="' + photo.media.m + '">' +
+																'</a>' +
+															'</li>';
+			});
+			thumbnailTemplate += '</ul>';
+		}*/
+
+		$.getJSON(flickerApiUrl, flickerData, function(data) {
+			// storing the ul element
+			var thumbnailTemplate = '<ul>';
+			// .each() will iterate throughitem in the returned json data [] and apply the callback function to each item
+			// Data is the is the returned JSON {}
+			// I need to access the items property of the data {}, it holds all of the photo data in a []
+			$.each(data.items, function(index, photo){
+				// Stores the values for each photo in this thumbnail template
+				thumbnailTemplate += '<li class="grid-25 tablet-grid-50">' +
+																'<a href="' + photo.link +'" class="image">' +
+																	'<img src="' + photo.media.m + '">' +
+																'</a>' +
+															'</li>';
+			});
+			thumbnailTemplate += '</ul>';
+			$('#photos').html(thumbnailTemplate);
 		});
 	});
 });
